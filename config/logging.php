bd858b577a2fc820a11b99859c2edcdc2c1855ac
didinkaj <didinkaj@gmail.com>
2018-05-17 12:08:32 +0300

    error logging

--- config/logging.php
+++ config/logging.php
@@ -44,38 +44,38 @@ return [
             'level' => 'debug',
         ],
 
-        'daily' => [
-            'driver' => 'daily',
-            'path' => storage_path('logs/laravel.log'),
-            'level' => 'debug',
-            'days' => 7,
-        ],
-
-        'slack' => [
-            'driver' => 'slack',
-            'url' => env('LOG_SLACK_WEBHOOK_URL'),
-            'username' => 'Laravel Log',
-            'emoji' => ':boom:',
-            'level' => 'critical',
-        ],
-
-        'stderr' => [
-            'driver' => 'monolog',
-            'handler' => StreamHandler::class,
-            'with' => [
-                'stream' => 'php://stderr',
-            ],
-        ],
-
-        'syslog' => [
-            'driver' => 'syslog',
-            'level' => 'debug',
-        ],
-
-        'errorlog' => [
-            'driver' => 'errorlog',
-            'level' => 'debug',
-        ],
+        // 'daily' => [
+            // 'driver' => 'daily',
+            // 'path' => storage_path('logs/laravel.log'),
+            // 'level' => 'debug',
+            // 'days' => 7,
+        // ],
+// 
+        // 'slack' => [
+            // 'driver' => 'slack',
+            // 'url' => env('LOG_SLACK_WEBHOOK_URL'),
+            // 'username' => 'Laravel Log',
+            // 'emoji' => ':boom:',
+            // 'level' => 'critical',
+        // ],
+// 
+        // 'stderr' => [
+            // 'driver' => 'monolog',
+            // 'handler' => StreamHandler::class,
+            // 'with' => [
+                // 'stream' => 'php://stderr',
+            // ],
+        // ],
+// 
+        // 'syslog' => [
+            // 'driver' => 'syslog',
+            // 'level' => 'debug',
+        // ],
+// 
+        // 'errorlog' => [
+            // 'driver' => 'errorlog',
+            // 'level' => 'debug',
+        // ],
     ],
 
 ];
